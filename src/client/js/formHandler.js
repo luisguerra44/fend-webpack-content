deda50c27a898316596c577a207c004e2b369a73
luis guerra <luis@Tests-MacBook-Air.local>
2020-02-28 19:02:21 -0600

    all my changes done

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,36 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let text = document.getElementById('name').value
+
+    Client.text
+    
+
+        console.log(text)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    fetch('http://localhost:8080/api',{
+        method: 'POST',
+       
+        body: JSON.stringify({text}),
+        headers: {"Content-Type": "application/json"}
     })
+    .then(res => res.json())
+    .then(function(data) {
+        console.log("Updating the UI!");
+        
+        document.getElementById('text').innerHTML = `text: ${data.text} <br>`
+        document.getElementById('polarity').innerHTML = `polarity: ${data.polarity} <br>`
+        document.getElementById('polarity_confidence').innerHTML = `polarity confidence: ${data.polarity_confidence} <br>`
+        document.getElementById('subjectivity').innerHTML = `subjectivity: ${data.subjectivity} <br>`
+        document.getElementById('subjectivity_confidence').innerHTML = `subjectivity confidence: ${data.subjectivity_confidence}`
+
+        console.log(data);
+        
+    }).catch(err => console.log(err));
+
 }
+
+
 
 export { handleSubmit }
